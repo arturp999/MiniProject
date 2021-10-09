86d7f57bb6632b3830e90e246d3532cbaa9221a1
Artur Pereira <arturpe95@gmail.com>
2021-10-09 11:20:23 +0100

    up

---
+++ MiniProject/PaginationLibrary/PagedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationLibrary
+{
+    public class PagedList<T>: List<T>
+    {
+        private int CurrentPage { get; set; }
+        private int TotalPages { get; set; }
+        private int PageSize { get; set; }
+        private int TotalCount { get; set; }
+        private bool HasPrevious => CurrentPage > 1;
+        private bool HasNext => CurrentPage < TotalPages;
+
+        public PagedList() { }
+
+        public PagedList( List<T> items, int count, int pageNumber, int pageSize) {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+
+        public object GetMetaData() {
+            return new
+            {
+                CurrentPage,
+                TotalPages,
+                PageSize,
+                TotalCount,
+                HasPrevious,
+                HasNext
+            };
+        }
+    }
+}
---
+++ MiniProject/PaginationLibrary/PaginationExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace PaginationLibrary
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class PaginationExtensions
+    {
+        public static PagedList<T> ToPagedList<T>(this List<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+        public static PagedList<T> ToPagedList<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
